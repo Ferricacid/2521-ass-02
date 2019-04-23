32e72a214f801b61453caae6053fb8a9557dd30b
redpuffles <regor.wu@gmail.com>
2019-04-24 01:00:13 +1000

    addpq

--- ass02/PQ.c
+++ ass02/PQ.c
@@ -10,8 +10,7 @@
 
 // Structs.
 struct PQRep {
-	int isEmpty;
-	ItemPQ node;
+	ItemPQ *node;
 	struct PQRep *next;
 };
 
@@ -19,11 +18,7 @@ struct PQRep {
 PQ newPQ() {
 	PQ new = malloc(sizeof(PQ));
 	assert(new != NULL);
-
-	ItemPQ empty = {0};
-
-	new->isEmpty = 1;
-	new->node = empty;
+	new->node = NULL;
 	new->next = NULL;
 
 	return new;
@@ -32,16 +27,51 @@ PQ newPQ() {
 int PQEmpty(PQ pq) {
 	assert(pq != NULL);
 
-	return pq->isEmpty;
+	int isEmpty;
+
+	if (pq->node == NULL) {
+		isEmpty = 1;
+	}
+	else {
+		isEmpty = 0;
+	}
+
+	return isEmpty;
 }
 
 void addPQ(PQ pq, ItemPQ element) {
 	assert(pq != NULL);
 
 	// Empty PQ.
-	// if (pq->isEmpty == 1) {
-	// 	pq->node = element;
-	// }
+	if (pq->node == NULL) {
+		pq->node = malloc(sizeof(ItemPQ*))
+		assert(pq->node != NULL);
+		pq->node->key = element.key;
+		pq->node->value = element.value;
+		pq->next = NULL;
+	}
+	// At least one item.
+	else {
+		PQ curr = pq;
+		// Check if exists already.
+		while (curr != NULL) {
+			if (curr->node->key == element.key) {
+				curr->node->value = element.value;
+				return;
+			}
+			curr = curr->next;
+		}
+
+		// Not in PQ, add to end.
+		curr = pq;
+		while (curr->next != NULL) {
+			curr = curr->next;
+		}
+		curr->next = newPQ();
+		curr->next->node = malloc(sizeof(ItemPQ*));
+		curr->next->node->key = element.key;
+		curr->next->node->value = element.value;
+	}
 }
 
 ItemPQ dequeuePQ(PQ pq) {
